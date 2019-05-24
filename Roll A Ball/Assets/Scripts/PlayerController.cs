b13fe16a0bfe568a126b7ab72b18037267ae6499
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-05-24 17:46:31 +0800

    3 OF 8
    Camera movement,player and physics done

---
+++ Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVerti = Input.GetAxis("Vertical");
+
+        Vector3 moveMent = new Vector3(moveHori,0.0f,moveVerti);
+        rb.AddForce(moveMent * speed);
+    }
+}
