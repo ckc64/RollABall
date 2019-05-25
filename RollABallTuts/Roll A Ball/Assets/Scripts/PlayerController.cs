26788f9bd25865168e25738375930e3898e67ef3
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2019-05-25 14:59:36 +0800

    With Build Application
    Compete

---
+++ RollABallTuts/Roll A Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int count;
+    public float speed;
+
+    public Text countText;
+    public Text winText;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        winText.text = "";
+        setCountText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHori = Input.GetAxis("Horizontal");
+        float moveVerti = Input.GetAxis("Vertical");
+
+        Vector3 moveMent = new Vector3(moveHori,0.0f,moveVerti);
+        rb.AddForce(moveMent * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+         
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count += 1;
+            setCountText();
+        }
+    }
+
+
+    void setCountText() {
+        countText.text = "Count : " + count.ToString();
+
+        if (count == 13) {
+            winText.text = "YOU WIN";
+        }
+    }
+}
